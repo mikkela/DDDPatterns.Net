627088fd4645d856c4374dd43164beb037844b8f
Mikkel Arentoft <mail@mikkelarentoft.net>
2016-02-05 10:26:38 +0100

    Added foundation

---
+++ Mikadocs.DDDPatterns/GloballyUniqueEntityIdentity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+
+namespace Mikadocs.DDDPatterns
+{
+    [Serializable]
+    public abstract class GloballyUniqueEntityIdentity : ValueObject<Guid>
+    {
+        protected GloballyUniqueEntityIdentity(Guid value) : base(value)
+        {
+        }
+    }
+
+    public static class GloballyUniqueEntityIdentityFactory
+    {
+        public static T CreateNewEntityIdentity<T>() where T : GloballyUniqueEntityIdentity
+        {
+            return CreateEntityIdentity<T>(Guid.NewGuid());
+        }
+
+        public static T CreateEntityIdentity<T>(Guid value) where T : GloballyUniqueEntityIdentity
+        {
+            return (T)
+                typeof(T).GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public, null,
+                    new[] { typeof(Guid) }, null).Invoke(new object[] { value });
+        }
+    }
+}
---
+++ Mikadocs.DDDPatterns/NumberBasedEntityIdentity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+
+namespace Mikadocs.DDDPatterns
+{
+    [Serializable]
+    public abstract class NumberBasedEntityIdentity : ValueObject<long>
+    {
+        protected NumberBasedEntityIdentity(long value) : base(value)
+        {
+        }
+    }
+
+    public static class NumberBasedEntityIdentityFactory
+    {
+        public static T CreateEntityIdentity<T>(long value) where T : NumberBasedEntityIdentity
+        {
+            return (T)
+                typeof(T).GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public, null,
+                    new[] { typeof(long) }, null).Invoke(new object[] { value });
+        }
+    }
+}
