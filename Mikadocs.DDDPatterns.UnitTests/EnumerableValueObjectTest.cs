d7ad6bc085cf6a49db95fc6fae0a4790c9827c06
Mikkel Arentoft <mail@mikkelarentoft.net>
2016-02-08 21:53:39 +0100

    Added Enumerable Value Objects

---
+++ Mikadocs.DDDPatterns.UnitTests/EnumerableValueObjectTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Mikadocs.DDDPatterns.UnitTests
+{
+    class EnumerableValueObjectTest
+    {
+        class ArrayValueObject : EnumerableValueObject<int>
+        {
+            public ArrayValueObject(int[] value1) : base(value1)
+            {
+            }
+        }
+
+        [Test]
+        public void Given_two_sets_with_same_elements_When_they_are_compared_Then_they_are_considered_equal()
+        {
+            var sut1 = new ArrayValueObject(new[] {1, 2, 3});
+            var sut2 = new ArrayValueObject(new[] { 1, 2, 3 });
+
+            Assert.IsTrue(sut1.Equals(sut2));
+            Assert.IsTrue(sut2.Equals(sut1));
+            Assert.IsTrue(sut1.GetHashCode() == sut2.GetHashCode());
+        }
+
+        [Test]
+        public void Given_two_sets_with_different_elements_When_they_are_compared_Then_they_are_not_considered_equal()
+        {
+            var sut1 = new ArrayValueObject(new[] { 1, 2, 3 });
+            var sut2 = new ArrayValueObject(new[] { 1, 2, 4 });
+
+            Assert.IsFalse(sut1.Equals(sut2));
+            Assert.IsFalse(sut2.Equals(sut1));
+            Assert.IsFalse(sut1.GetHashCode() == sut2.GetHashCode());
+        }
+
+        [Test]
+        public void Given_a_set_When_an_EnumberableValueObject_is_created_from_the_set_Then_the_value_object_can_enumerate_the_set()
+        {
+            var set = new[] {1, 2, 3};
+
+            var sut = new ArrayValueObject(set);
+
+            CollectionAssert.AreEqual(set, sut);
+        }
+    }
+}
---
+++ Mikadocs.DDDPatterns/EnumerableValueObject.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mikadocs.DDDPatterns
+{
+    public class EnumerableValueObject<T> : ValueObject<IEnumerable<T>>, IEnumerable<T>
+    {
+        public EnumerableValueObject(IEnumerable<T> value1) : base(value1)
+        {
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return Value1.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public override bool Equals(ValueObject<IEnumerable<T>> other)
+        {
+            return Value1.SequenceEqual(other.Value1);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return Value1.Aggregate(19, (current, v) => current * 31 + v.GetHashCode());
+            }
+        }
+    }
+}
